6e5c23fcd3e97c78b81ff706966de5047fe4ba29
Campaign AI <162702434+CampaignAI@users.noreply.github.com>
2024-03-08 13:22:35 +0100

    Add files via upload

---
+++ Credentials.php
@@ -0,0 +1,19 @@
+<?php
+
+class Credentials{
+    public static $MASTER_ID = '4610623388';    
+    public static $ACCOUNT_ID = '3059122346';         
+    public static $PAGE_LIMIT = 500;
+    public static $MULTIPLIER = 1000000;
+    public static $REDIRECT_URI = '';
+    public static $AUTHORIZATION_URI = 'https://accounts.google.com/o/oauth2/v2/auth';
+    public static $TOKEN_CREDENTIAL_URI = 'https://oauth2.googleapis.com/token';
+    public static $SCOPE = 'https://www.googleapis.com/auth/adwords';
+ 
+    //account
+    public static $CLIENT_ID = '895404966319-56k2jgmoc76gh690tmrfpldqdcdl9gj4.apps.googleusercontent.com';
+    public static $CLIENT_SECRET = 'BPfvlKXfRtMR8LKgEnR6ZQdN';
+    public static $DEVELOPER_TOKEN = '164oteZ_x-t9PxaFy1Mikg';
+    public static $REFRESH_TOKEN = '1//09OaPWX6G5uICCgYIARAAGAkSNwF-L9Irjjgo9MPtQ_r1DDPdPYYLcQQB10tW4cAQyQuFBz5AkADicEA-tETN4irUVFeb1M_UfWE';
+            
+}
